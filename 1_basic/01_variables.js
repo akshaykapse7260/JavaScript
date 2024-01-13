2cef378524d4ff5ac27f2dd6d6685c4de7364f5e
Akshay Kapse <107935860+akshaykapse7260@users.noreply.github.com>
2024-01-13 13:21:48 +0000

    Conversion of data types

---
+++ 1_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accId = 12344567
+let accEmail = "akshay@gmail.com"
+var accPass = "1212"
+accCity = "Pune"
+let accState;
+console.table([accId,accEmail,accPass,accCity,accState]);
+// accId = 1234534;  //This is not allowed here because this variable is declared with const data type
+accEmail="kapse@gmail.com";
+accPass = 4545;
+accCity = "Latur";
+accState = "Maharashtra";
+console.table([accId,accEmail,accPass,accCity,accState]);
---
+++ 1_basic/conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "Akshay"
+//console.log(typeof score)
+//console.log(typeof(score))
+//console.log(score)
+let valueInNumber = Number(score)
+
+//console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+//console.log(typeof(valueInNumber))
+
+let isLoggedIn = " ";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+// 1 => true; 0 => false
+//"" => false; "AK" => true; " " => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
