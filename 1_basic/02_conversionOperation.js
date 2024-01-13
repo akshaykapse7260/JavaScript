eda4af4ad32c7b6971bbd4d71ac94d3573dedd6a
Akshay Kapse <107935860+akshaykapse7260@users.noreply.github.com>
2024-01-13 17:48:19 +0000

    comparison

---
+++ 1_basic/02_conversionOperation.js
@@ -0,0 +1,49 @@
+let score = "Akshay"
+//console.log(typeof score)
+//console.log(typeof(score))
+//console.log(score)
+let valueInNumber = Number(score)
+
+//console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+//console.log(typeof(valueInNumber))
+
+let isLoggedIn = " ";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+// 1 => true; 0 => false
+//"" => false; "AK" => true; " " => true
+
+let someNumber = 33
+//console.log(someNumber)
+//console.log(typeof someNumber)
+let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+//********************OPERATION***********************/
+let val = 3;
+let negVal = -val;
+//console.log(negVal);
+
+//console.log(2**3); // it is 2 to the power 3 and out put is 8.
+let str1 = "Hello"
+let str2 = " Akshay"
+let a=10;
+let b=20;
+
+//let str3 = str1+str2+a+b //Hello Akshay1020
+//let str3 = a+b+str1+str2 //30Hello Akshay
+//console.log(str3)
+//console.log(+true) //op is 1
+//console.log(+" ") //op is 0
+let num1,num2,num3
+num1=num2=num3=2+2
+// console.log("num1: "+num1)  //4
+// console.log("num2: "+num2)  //4
+// console.log("num3: "+num3)  //4
+
+let gc = 100;
+gc++;
+console.log("gc: "+gc)
