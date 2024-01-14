a1c5602192bae301f198fc891e4e68d5019cfcf0
Akshay Kapse <107935860+akshaykapse7260@users.noreply.github.com>
2024-01-14 11:10:10 +0000

    string manuplation

---
+++ 1_basic/05_strings.js
@@ -0,0 +1,27 @@
+const name = "Akshay"
+const repoCount = 50;
+// console.log(name + repoCount + "Value");
+console.log(`Hello my Name is ${name.toLowerCase()} and my repo count is ${repoCount}`);
+
+const gName = new String('Kapse');
+//console.log(gName[3]);
+//console.log(gName.__proto__);
+//console.log(gName.charAt(2));
+//console.log(gName.indexOf('p'));
+//const newString = gName.substring(1,4);
+//console.log(newString);
+
+const anotherString = gName.slice(-5,3);
+console.log(anotherString);
+
+const newStringOne = "   Akshay   ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+const url = "https://akshay.com/akshay%20kapse";
+console.log(url.replace('%20','_'));
+
+console.log(url.includes('kapse'))
+console.log(url.includes('20%'))
+
+const newStringTwo = new String('Akshay-Tanaji-Kapse');
+console.log((newStringTwo.split('-'))[0])
