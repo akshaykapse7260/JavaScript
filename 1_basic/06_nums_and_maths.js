2d91e1d7204ebb34132c4b7dead8b121926e4b72
Akshay Kapse <107935860+akshaykapse7260@users.noreply.github.com>
2024-01-15 10:29:05 +0000

    more about Math. methods

--- 1_basic/06_nums_and_maths.js
+++ 1_basic/06_nums_and_maths.js
@@ -1,13 +1,33 @@
 const score = 100;
-console.log(score)
+//console.log(score)
 
 const bal = new Number(100)
-console.log(bal);
+//console.log(bal);
 
-console.log(bal.toString().length)
-console.log(bal.toFixed(3))
+//console.log(bal.toString().length)
+//console.log(bal.toFixed(3))
 
 const num2 = 123.8966
-console.log(num2.toPrecision(4))
+//console.log(num2.toPrecision(4))
 const num3 = 1000000
-console.log(num3.toLocaleString('en-IN'))
+//console.log(num3.toLocaleString('en-IN'))
+
+//+++++++++++ Maths ++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.sqrt(49));
+// console.log(Math.pow(2,3));
+// console.log(Math.min(2,4,7,4,1,9));
+// console.log(Math.max(2,4,7,4,2,9));
+//console.log(Math.random());
+//console.log((Math.random()*10)+1);
+//console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
