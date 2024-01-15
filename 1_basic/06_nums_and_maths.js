41e10fd46cfb5bc88ded2ddb7bf88f621374d865
Akshay Kapse <107935860+akshaykapse7260@users.noreply.github.com>
2024-01-15 10:09:36 +0000

    nums and maths

---
+++ 1_basic/06_nums_and_maths.js
@@ -0,0 +1,13 @@
+const score = 100;
+console.log(score)
+
+const bal = new Number(100)
+console.log(bal);
+
+console.log(bal.toString().length)
+console.log(bal.toFixed(3))
+
+const num2 = 123.8966
+console.log(num2.toPrecision(4))
+const num3 = 1000000
+console.log(num3.toLocaleString('en-IN'))
